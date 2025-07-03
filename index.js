30322316888e055309b93ee0b090a9fa0e422afe
yenmaylee <yen.lee@bnm.gov.my>
2025-07-04 06:12:15 +0800

    Create session if correct credentials

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const session = require('express-session')
 
 const app = express()
 
@@ -7,6 +8,13 @@ app.set('view engine', 'ejs')
 // parses url encoded payload, i.e. data sent with Content-Type: application/x-www-form-urlencoded
 // extended: false to use query training library
 app.use(express.urlencoded({ extended: false }))
+
+app.use(session({
+    secret: 'my_session_secret',
+    resave: true,
+    saveUninitialized: false,
+    name: 'yenmay' // name the cookie
+}))
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -21,7 +29,10 @@ app.post('/login', (req, res) => {
     // console.log(req.body)
     // simulate having a DB
     if (req.body.username === "beep" && req.body.password === "123") {
-        // create a session
+        // Create session and store user logged details, do not store sensitive info in the session e.g. password
+        req.session.user = { id: 1, username: 'beep', name: 'beep' }
+        // If credentials match then redirect user to home
+        res.redirect('/')
     } else {
         // render the login.ejs view engine, pass error as value to that template
         res.render('login', {error: "Wrong credentials"})
