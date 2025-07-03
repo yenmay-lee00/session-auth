67d641b50ee59f23c51999ca9125f08b84d8ea1f
yenmaylee <yen.lee@bnm.gov.my>
2025-07-04 01:44:51 +0800

    Create login page

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # session-auth
+
+Tutorial: https://www.youtube.com/watch?v=-ebXpRi1yQg&t
--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const express = require('express')
+
 const app = express()
+
 app.set('view engine', 'ejs')
+
+// parses url encoded payload, i.e. data sent with Content-Type: application/x-www-form-urlencoded
+// extended: false to use query training library
+app.use(express.urlencoded({ extended: false }))
+
 app.get('/', (req, res) => {
     res.render('home')
 })
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.post('/login', (req, res) => {
+    console.log(req.body)
+})
+
 app.listen(3000, () => console.log('Server started on port 3000'))
+ 
